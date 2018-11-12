a40d03bebe60c246334a7da04925c33d8418a401
JoanChirinos <jchirinos@stuy.edu>
2018-11-12 16:31:37 -0500

    .

--- run.py
+++ run.py
@@ -4,15 +4,15 @@ def go():
     student_id = 211938341
     while (True):
         print(student_id)
-        for i in range(20):
+##        for i in range(20):
 ##            webbrowser.open('http://homer.stuy.edu/~jchirinos/GetTalosPics/get_imgs.py?student_id={}'.format(student_id), new=2)
-            url = 'http://homer.stuy.edu/~jchirinos/GetTalosPics/get_imgs.py?student_id={}'.format(student_id)
-            try:
-                urllib.request.urlopen(url)
-            except Exception as e:
-                print(e)
-                print('FAILED AT ~ {}'.format(student_id))
-            student_id += 1
+        url = 'http://homer.stuy.edu/~jchirinos/GetTalosPics/get_imgs.py?student_id={}'.format(student_id)
+        try:
+            urllib.request.urlopen(url)
+        except Exception as e:
+            print(e)
+            print('FAILED AT ~ {}'.format(student_id))
+        student_id += 1
 ##        time.sleep(18)
 ##        os.system("killall -9 'Google Chrome'")
 
