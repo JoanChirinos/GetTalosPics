8169637597ab933ac7ccb7d6825f5386ef326b9c
JoanChirinos <jchirinos@stuy.edu>
2018-11-12 01:31:43 -0500

    .

---
+++ get_imgs.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+
+import urllib
+import cgi, cgitb
+cgitb.enable()
+
+print('Content-type: text/html\n\n')
+
+def get_image(student_id):
+    try:
+        url = 'https://talos.stuy.edu/static/img/id_pics/{}.jpg'.format(student_id)
+        resource = urllib.urlopen(url)
+        #print(student_id)
+
+        stuff_read = resource.read()
+
+        #print('{}\n\n'.format(stuff_read))
+
+        if 'not found on this server' in stuff_read:
+            #print('failed')
+            return
+        
+        output = open("pics/{}.jpg".format(student_id),"wb")
+        output.write(stuff_read)
+        output.close()
+    except:
+        print('failed')
+        print(student_id)
+        return
+
+def main():
+    student_id = 210032785
+    fs = cgi.FieldStorage()
+    new_id = fs.getvalue('student_id', 210032785)
+    student_id = int(new_id)
+    while (True):
+        get_image(student_id)
+        student_id += 1
+    print('<br><br>Thanks')
+    
+
+main()
+##get_image(209993641)
+##get_image(209993642)
