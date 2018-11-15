6e167fbeece0de84452889dda7c32a0baee5b1a4
JoanChirinos <jchirinos@stuy.edu>
2018-11-15 12:37:05 -0500

    .

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-import os, webbrowser, time, urllib.request
+import os, webbrowser, time, urllib.request, sys
 
-def go():
+def go(sid):
 ##    student_id = 211938341
-    student_id = 212115686
+##    student_id = 212115686
+    student_id = int(sid)
+    max = student_id + 1000
     while (True):
-        print(student_id)
 ##        for i in range(20):
 ##            webbrowser.open('http://homer.stuy.edu/~jchirinos/GetTalosPics/get_imgs.py?student_id={}'.format(student_id), new=2)
         url = 'http://homer.stuy.edu/~jchirinos/GetTalosPics/get_imgs.py?student_id={}'.format(student_id)
@@ -16,7 +17,7 @@ def go():
         student_id += 1
 ##        time.sleep(18)
 ##        os.system("killall -9 'Google Chrome'")
-
+    print(student_id)
 
 def new_go():
     student_id = 209993642
@@ -24,4 +25,4 @@ def new_go():
     f = urllib.request.urlopen(url)
     print(f.read())
 
-go()
+go(sys.argv[1])
